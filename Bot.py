78bec4d5888182d65c7c864b6537194ccbf6ab69
ReritoO-dev <77019976+ReritoO-dev@users.noreply.github.com>
2021-01-08 02:00:34 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,21 +1,21 @@
+from discord.ext import commands
 import discord
 import asyncio
-from discord.ext import commands
 from utils import *
 
 TOKEN = TOKEN
-bot = commands.Bot(command_prefix="r!!", case_insentitive=True)
+bot = commands.Bot(command_prefix="r!", case_insentitive=True)
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+  await bot.change_presence(status=discord.Status.online, activity=discord.Activity(
+type=discord.ActivityType.streaming, name="Test", url="https://twitch.com/twitch"))
+  print('girildi.')
+  print('reritobot!')
 
-@bot.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def yardim(ctx):
+  await ctx.send("gelcek onlar")
 
 bot.run(TOKEN)
